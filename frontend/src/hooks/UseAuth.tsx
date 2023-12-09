5ab55f16dd17b0de65ff5245eb3c85cf53e31527
spockwall <abcdefg35216874@gmail.com>
2023-12-10 02:45:48 +0800

    fix: parking space hove bug

--- frontend/src/hooks/UseAuth.tsx
+++ frontend/src/hooks/UseAuth.tsx
@@ -1,16 +1,18 @@
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
-import type { roleType } from "@/types";
+import type { roleType } from "../types";
+import { AUTHACTION } from "../reducers/authReducer";
+import type { authState } from "../types";
 
 export default function useAuth() {
     const { authState, authDispatch } = useContext(AuthContext);
 
     const login = (token: string, role: roleType) => {
-        authDispatch({ type: "LOGIN", payload: { token, role } });
+        authDispatch({ type: AUTHACTION.LOGIN, payload: { token, role } });
     };
 
     const logout = () => {
-        authDispatch({ type: "LOGOUT" });
+        authDispatch({ type: AUTHACTION.LOGOUT, payload: {} as authState });
     };
     return { authState, login, logout };
 }
